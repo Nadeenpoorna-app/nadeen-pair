2fbe6173c7e4df929d1b3d579e1fc10e781e00d3
Nadeen Poorna <nadeenpoorna405@gmail.com>
2025-11-19 20:49:57 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -174,7 +174,7 @@ if (fs.existsSync('./auth_info_baileys')) {
 
 
 
-                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+                        const string_session = mega_url.replace('https://mega.nz/file/', '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=');
 
 
 
